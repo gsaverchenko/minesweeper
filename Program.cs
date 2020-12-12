fa5c8a1c9816e2650896914a541cdb0e9933be88
Greg <gsaverchenko@users.noreply.github.com>
2020-12-12 13:29:56 -0800

    Hard coded mines to 5 and 8.

--- Program.cs
+++ Program.cs
@@ -6,7 +6,26 @@ namespace minesweeper
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var map = new int[10];
+            map[4] = 1;
+            map[7] = 1;
+            var answer = string.Empty;
+            Console.WriteLine("Type a number 1-10");
+            
+            while (answer.ToUpper() != "EXIT")
+            {
+                int guess;
+                answer = Console.ReadLine();
+                var result = int.TryParse(answer, out guess);
+                if (guess == 5 || guess == 8)
+                {
+                    Console.WriteLine("BOOM!!!");
+                }
+                else
+                {
+                    Console.WriteLine("You're safe :D");
+                }
+            }
         }
     }
 }
